c411c907879f4ef4c404f978f3a890b642c83372
Michel Alonso <michel.s.alonso@gmail.com>
2017-11-25 21:19:53 -0200

    Remove renderer src

--- src/js/controllers/navbar.js
+++
@@ -1,9 +0,0 @@
-app.controller('NavbarController', function($element, $rootScope) {
-  // $element.hide();
-  $rootScope.$on('hideNavbar', function() {
-    $element.hide();
-  });
-  $rootScope.$on('showNavbar', function() {
-    $element.show();
-  });
-});
